70fc07226107144127265765e31a226520445923
Eugene <enot8826@gmail.com>
2020-11-30 13:25:25 -0800

    Daily commit! Logger class

---
+++ Logger/handler.py
@@ -0,0 +1,26 @@
+import logging
+import os
+
+
+class LoggerIt:
+    def __init__(self):
+        __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        error_message = os.path.join(__location__, 'errors.log')
+        info_message = os.path.join(__location__, 'info.log')
+        self.error_logger = logging.getLogger('Time-Manager-API-Error')
+        fh = logging.FileHandler(error_message)
+        fh.setLevel(logging.ERROR)
+        fh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+        self.error_logger.addHandler(fh)
+
+        self.info_logger = logging.getLogger('Time-Manager-API-Info')
+        fh = logging.FileHandler(info_message)
+        fh.setLevel(logging.WARNING)
+        fh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+        self.info_logger.addHandler(fh)
+
+    def write_error(self, msg='Something went wrong'):
+        self.error_logger.error(msg)
+
+    def write_info(self, msg='Everything goes normal'):
+        self.info_logger.warning(msg)
