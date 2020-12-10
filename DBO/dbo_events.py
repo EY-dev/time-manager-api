f5c6f02d8dec7e2b8036eaa8ba18afa543734509
Eugene <enot8826@gmail.com>
2020-12-09 20:19:29 -0800

    Daily commit getEvents

--- DBO/dbo_events.py
+++ DBO/dbo_events.py
@@ -11,20 +11,21 @@ class DBOEvents(DBOHandler):
     def get_events(self, user_id, date):
         first_date = datetime.datetime.strptime(date, '%Y-%m-%d') - datetime.timedelta(days=7)
         last_date = datetime.datetime.strptime(date, '%Y-%m-%d') + datetime.timedelta(days=7)
-        sql = 'SELECT id, title, date, time, is_complete FROM to_do_list WHERE user_id = "{id}" AND ' \
-              'date between "{date1}" AND "{date2}" ORDER BY date, time'.format(id=user_id, date1=first_date, date2=last_date)
+        sql = 'SELECT id, title, date, time_from, time_to, is_complete FROM to_do_list WHERE user_id = "{id}" AND ' \
+              'date between "{date1}" AND "{date2}" ORDER BY date, time_from'.format(id=user_id, date1=first_date, date2=last_date)
         temp = self.select_data(sql)
-        logger = LoggerIt.get_instance()
-        logger.write_info('temp'+str(temp))
-        date = temp[0]['date'].strftime('%Y-%m-%d')
-        result = dict()
-        result[date] = []
-        for row in temp:
-            row['date'] = row['date'].strftime('%Y-%m-%d')
-            row['time'] = str(row['time'])
-            if row['date'] != date:
-                date = row['date']
-            result[date].append(row)
-
-        logger.write_info(str(result))
-        return result
+        if len(temp) == 0:
+            return {}
+        else:
+            date = temp[0]['date'].strftime('%Y-%m-%d')
+            result = dict()
+            result[date] = []
+            for row in temp:
+                row['date'] = row['date'].strftime('%Y-%m-%d')
+                row['time_from'] = str(row['time_from'])
+                row['time_to'] = str(row['time_to'])
+                if row['date'] != date:
+                    date = row['date']
+                    result[date] = []
+                result[date].append(row)
+            return result
